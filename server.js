f8ecb395a10f52e49878fa404703cae5d643dbb6
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-06-28 10:28:27 -0500

    ui changes

--- server.js
+++ server.js
@@ -13,17 +13,19 @@ mongoose.connect(mongoUri, {
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
+//Render the index.ejs file
 app.get("/", async (req, res) => {
   const shortUrls = await ShortUrl.find();
   res.render("index", { shortUrls: shortUrls });
 });
 
+//Create a new short URL
 app.post("/shortUrls", async (req, res) => {
   await ShortUrl.create({ full: req.body.fullUrl });
-  console.log(req.body.fullUrl);
   res.redirect("/");
 });
 
+//Redirect to the original URL
 app.get("/:shortUrl", async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
   if (shortUrl == null) return res.sendStatus(404);
@@ -31,6 +33,12 @@ app.get("/:shortUrl", async (req, res) => {
   shortUrl.save();
 
   res.redirect(shortUrl.full);
+});
+
+//Delete the short URL
+app.post("/:shortUrl", async (req, res) => {
+  await ShortUrl.findOneAndDelete({ short: req.params.shortUrl });
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 4000);
