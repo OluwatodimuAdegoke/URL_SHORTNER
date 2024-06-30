9c0203c86a6a68d97581d082ab7f626c3dc744ab
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-06-30 16:38:03 -0500

    original

--- index.js
+++ index.js
@@ -1,183 +1,179 @@
 const express = require("express");
-// const mongoose = require("mongoose");
-// const cors = require("cors");
-// const jwt = require("jsonwebtoken");
-// const bcrypt = require("bcryptjs");
-// const cookieParser = require("cookie-parser");
-// const ShortUrl = require("./models/shortUrl");
-// const User = require("./models/users");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
+const ShortUrl = require("./models/shortUrl");
+const User = require("./models/users");
 require("dotenv").config();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+mongoUri = process.env.MONGO_URI;
+tokenSecret = process.env.TOKEN_SECRET;
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-// mongoUri = process.env.MONGO_URI;
-// tokenSecret = process.env.TOKEN_SECRET;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ALLOWED_ORIGIN || "http://localhost:4000", // Fallback to localhost for development
+  })
+);
+app.use(express.urlencoded({ extended: false }));
 
-// mongoose.connect(mongoUri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+//Render the index.ejs file
+app.get("/", async (req, res) => {
+  try {
+    const cookie = req.cookies["jwt"];
+    if (!cookie) throw new Error("No cookie");
 
-// app.set("view engine", "ejs");
-// app.use(express.static("public"));
-// app.use(cookieParser());
-// app.use(express.json());
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: process.env.ALLOWED_ORIGIN || "http://localhost:4000", // Fallback to localhost for development
-//   })
-// );
-// app.use(express.urlencoded({ extended: false }));
+    const claims = jwt.verify(cookie, tokenSecret);
+    if (!claims) throw new Error("Invalid token");
 
-// //Render the index.ejs file
-// app.get("/", async (req, res) => {
-//   try {
-//     const cookie = req.cookies["jwt"];
-//     if (!cookie) throw new Error("No cookie");
+    const user = await User.findOne({ _id: claims._id });
+    if (!user) throw new Error("User not found");
 
-//     const claims = jwt.verify(cookie, tokenSecret);
-//     if (!claims) throw new Error("Invalid token");
+    const shortUrls = await ShortUrl.find({ user: user._id });
+    res.render("index", { shortUrls: shortUrls });
+  } catch (err) {
+    if (
+      err.message === "No cookie" ||
+      err.message === "Invalid token" ||
+      err.message === "User not found"
+    ) {
+      res.redirect("/login");
+    } else {
+      console.log(err);
+      res.redirect("/login");
+    }
+  }
+});
 
-//     const user = await User.findOne({ _id: claims._id });
-//     if (!user) throw new Error("User not found");
+//Register Routes
+app.get("/register", (req, res) => {
+  res.render("register", { error: req.query.error ? req.query.error : "" });
+});
+app.post("/register", async (req, res) => {
+  try {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
 
-//     const shortUrls = await ShortUrl.find({ user: user._id });
-//     res.render("index", { shortUrls: shortUrls });
-//   } catch (err) {
-//     if (
-//       err.message === "No cookie" ||
-//       err.message === "Invalid token" ||
-//       err.message === "User not found"
-//     ) {
-//       res.redirect("/login");
-//     } else {
-//       console.log(err);
-//       res.redirect("/login");
-//     }
-//   }
-// });
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-// //Register Routes
-// app.get("/register", (req, res) => {
-//   res.render("register", { error: req.query.error ? req.query.error : "" });
-// });
-// app.post("/register", async (req, res) => {
-//   try {
-//     const name = req.body.name;
-//     const email = req.body.email;
-//     const password = req.body.password;
+    const existingUser = await User.findOne({ email: email });
+    if (existingUser) throw new Error("User already exists");
 
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
 
-//     const existingUser = await User.findOne({ email: email });
-//     if (existingUser) throw new Error("User already exists");
+    if (user) {
+      res.redirect("/login?error=Successfully registered, please login");
+    } else {
+      throw new Error("User not created");
+    }
+  } catch (err) {
+    res.redirect("/register?error=" + err.message);
+  }
+});
 
-//     const user = await User.create({
-//       name: name,
-//       email: email,
-//       password: hashedPassword,
-//     });
+app.get("/logout", (req, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.redirect("/login");
+});
 
-//     if (user) {
-//       res.redirect("/login?error=Successfully registered, please login");
-//     } else {
-//       throw new Error("User not created");
-//     }
-//   } catch (err) {
-//     res.redirect("/register?error=" + err.message);
-//   }
-// });
+// Login Routes
+app.get("/login", (req, res) => {
+  res.render("login", { error: req.query.error ? req.query.error : "" });
+});
+app.post("/login", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-// app.get("/logout", (req, res) => {
-//   res.cookie("jwt", "", { maxAge: 0 });
-//   res.redirect("/login");
-// });
+    const user = await User.findOne({
+      email: email,
+    });
 
-// // Login Routes
-// app.get("/login", (req, res) => {
-//   res.render("login", { error: req.query.error ? req.query.error : "" });
-// });
-// app.post("/login", async (req, res) => {
-//   try {
-//     const email = req.body.email;
-//     const password = req.body.password;
+    if (!user) throw new Error("User not found");
 
-//     const user = await User.findOne({
-//       email: email,
-//     });
+    if (!(await bcrypt.compare(password, user.password)))
+      throw new Error("Invalid password");
 
-//     if (!user) throw new Error("User not found");
+    const token = jwt.sign({ _id: user._id }, tokenSecret);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
-//     if (!(await bcrypt.compare(password, user.password)))
-//       throw new Error("Invalid password");
+    res.redirect("/");
+  } catch (err) {
+    if (err.message === "User not found") {
+      res.redirect("/login?error=User not found");
+    } else {
+      res.redirect("/login?error=Invalid password");
+    }
+  }
+});
 
-//     const token = jwt.sign({ _id: user._id }, tokenSecret);
-//     res.cookie("jwt", token, {
-//       httpOnly: true,
-//       maxAge: 24 * 60 * 60 * 1000,
-//     });
+//Create a new short URL
+app.post("/shortUrls", async (req, res) => {
+  try {
+    const cookie = req.cookies["jwt"];
+    await ShortUrl.create({
+      full: req.body.fullUrl,
+      user: jwt.verify(cookie, tokenSecret)._id,
+    });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
 
-//     res.redirect("/");
-//   } catch (err) {
-//     if (err.message === "User not found") {
-//       res.redirect("/login?error=User not found");
-//     } else {
-//       res.redirect("/login?error=Invalid password");
-//     }
-//   }
-// });
+//Redirect to the original URL
+app.get("/:shortUrl", async (req, res) => {
+  try {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+    if (shortUrl == null) return res.sendStatus(404);
+    shortUrl.clicks++;
+    shortUrl.save();
+    res.redirect(shortUrl.full);
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
 
-// //Create a new short URL
-// app.post("/shortUrls", async (req, res) => {
-//   try {
-//     const cookie = req.cookies["jwt"];
-//     await ShortUrl.create({
-//       full: req.body.fullUrl,
-//       user: jwt.verify(cookie, tokenSecret)._id,
-//     });
-//     res.redirect("/");
-//   } catch (err) {
-//     console.log(err);
-//     res.redirect("/");
-//   }
-// });
+//Delete the short URL
+app.post("/:shortUrl", async (req, res) => {
+  try {
+    const cookie = req.cookies["jwt"];
+    await ShortUrl.findOneAndDelete({
+      short: req.params.shortUrl,
+      user: jwt.verify(cookie, tokenSecret)._id,
+    });
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/");
+  }
+});
 
-// //Redirect to the original URL
-// app.get("/:shortUrl", async (req, res) => {
-//   try {
-//     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-//     if (shortUrl == null) return res.sendStatus(404);
-//     shortUrl.clicks++;
-//     shortUrl.save();
-//     res.redirect(shortUrl.full);
-//   } catch (err) {
-//     console.log(err);
-//     res.redirect("/");
-//   }
-// });
+app.get("*", (req, res) => {
+  res.status(404).send("404 Not Found");
+});
 
-// //Delete the short URL
-// app.post("/:shortUrl", async (req, res) => {
-//   try {
-//     const cookie = req.cookies["jwt"];
-//     await ShortUrl.findOneAndDelete({
-//       short: req.params.shortUrl,
-//       user: jwt.verify(cookie, tokenSecret)._id,
-//     });
-//     res.redirect("/");
-//   } catch (err) {
-//     console.log(err);
-//     res.redirect("/");
-//   }
-// });
-
-// app.get("*", (req, res) => {
-//   res.status(404).send("404 Not Found");
-// });
-
-// app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000);
